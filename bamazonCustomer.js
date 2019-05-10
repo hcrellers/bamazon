867138c2e783cecb06bed5373234b921ed8d8279
Halie <halie.creollo@gmail.com>
2019-05-10 14:06:58 -0500

    one of the final drafts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -54,30 +54,76 @@ function start() {
         name: "quantity",
         message: "How many units would you like to buy?"
       }
-    ]).then(function(answer) {
-     console.log(answer.id);
-     console.log(answer.quantity);
-     connection.query("SELECT * FROM products WHERE ?", { id: answer.id }, function(err, res) {
-    console.log("Quantity: " + res[0].stock_quantity);
-    //  console.log(res)
-    if (answer.quantity < res[0].stock_quantity) {
-      res[0].stock_quantity - answer.quantity;
-      //return value 
+    ]).then(function (answer) {
 
-    }
+      connection.query("SELECT * FROM products WHERE ?", { id: answer.id }, function (err, res) {
+        console.log("Purchasing: " + res[0].product_name);
+        console.log("How many: " + answer.quantity);
+     //  if (answer.id === id) {
+     //    start()
+      // }
+        //  function confirm() {
+        inquirer
+          .prompt([
+            {
+              type: "confirm",
+              message: "Are you sure:",
+              name: "confirm",
+              default: true
+            }
+          ]).then(function (response) {
+            // console.log(response.confirm);
+            if (response.confirm === true) {
+              console.log("Checking quantity...")
+              if (answer.quantity < res[0].stock_quantity) {
+                var checkStore = res[0].stock_quantity - answer.quantity;
+                 console.log(checkStore)
+                console.log("Transaction Complete");
+                connection.query("UPDATE products SET ? WHERE ?",
+                  [
+                    {
+                      stock_quantity: checkStore
+                    },
+                    {
+                      id: answer.id
+                    }
+                  ],
 
+                  function (err, res) {
+                    console.log(res.affectedRows + " products updated!\n");
+                    tableUpdate()
+                  });
+              }
+              else {
+                console.log("Insufficent quantity!\n");
+                console.log("Quantity in stock: " + res[0].stock_quantity);
+                tableUpdate()
+              }
+            }
 
-  });
-      
+          });
 
-      
       });
 
     });
-  }
 
-        
- 
+  });
 
+}
+function tableUpdate() {
+  connection.query("SELECT * FROM products", function (err, results) {
+    if (err) throw err;
+    //console.log(results);
+    results.forEach(row => {
+      // console.log(row)
+     products.push(
+       [row.id, row.product_name, row.department_name, row.price, row.stock_quantity]
 
+      );
 
+    });
+    console.log(products.toString());
+    start();
+  });
+
+}
